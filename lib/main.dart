2bca8918d306950be97883696e1ecf28e223b402
RAWMIC17 <hks1710harsh@gmail.com>
2023-06-22 13:43:56 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  // ignore: prefer_const_constructors
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+        child: Center(
+          child: Container(
+            child: Text("30 days of flutter"),
+          ),
+        ),
+      ),
+    );
+  }
+}
