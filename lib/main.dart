6c7a4e96175eb0f248031eb336e7fdab2a9ea2e3
RAWMIC17 <hks1710harsh@gmail.com>
2023-06-26 17:40:45 +0530

    Gay 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/home_page.dart';
+import 'package:flutter_application_1/pages/home_page.dart';
 
 void main() {
   // ignore: prefer_const_constructors
