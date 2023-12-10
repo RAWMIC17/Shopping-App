6d75d1aa6293560992aace29442d8b899996ff6f
RAWMIC17 <hks1710harsh@gmail.com>
2023-12-10 13:05:06 +0530

    day 20 completed

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/models/cart.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 class CartPage extends StatelessWidget {
@@ -30,17 +31,19 @@ class _CartTotal extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final _cart = CartModel();
     return SizedBox(
       height: 200,
       child: Row(
           mainAxisAlignment:
               MainAxisAlignment.spaceAround, //better than spacebetween
           children: [
-            "\$9999".text.xl4.color(context.theme.colorScheme.secondary).make(),
+            "\$${_cart.totalPrice}".text.xl4.color(context.theme.colorScheme.secondary).make(),
             30.widthBox,
             ElevatedButton(
                     onPressed: () {
-                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(//To display system message
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                        //To display system message
                         content: "Buying not supported yet.".text.make(),
                       ));
                     },
@@ -63,10 +66,11 @@ class _CartList extends StatefulWidget {
 }
 
 class _CartListState extends State<_CartList> {
+  final _cart = CartModel();
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: 5,
+      itemCount: _cart.items?.length,
       itemBuilder: (context, index) => ListTile(
         leading: Icon(Icons.done),
         trailing: IconButton(
@@ -75,7 +79,7 @@ class _CartListState extends State<_CartList> {
               Icons.remove_circle_outline_outlined,
               color: Colors.red,
             )),
-        title: "Item 1".text.make(),
+        title: _cart.items[index].name.text.make(),
       ),
     );
   }
